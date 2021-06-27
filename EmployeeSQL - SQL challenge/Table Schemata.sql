0c5652c425d69dab99fb5731f33a6f40e5420a3a
Melat Bekele <27melat28@gmail.com>
2021-06-27 03:31:40 -0400

    new lines

--- EmployeeSQL - SQL challenge/Table Schemata.sql
+++ EmployeeSQL - SQL challenge/Table Schemata.sql
@@ -1,4 +1,4 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
@@ -30,10 +30,10 @@ CREATE TABLE "employees" (
     "emp_no" int   NOT NULL,
     "emp_title_id" varchar(200)   NOT NULL,
     "birth_date" varchar(200)   NOT NULL,
-    "first_name" string   NOT NULL,
-    "last_name" string   NOT NULL,
-    "sex" string   NOT NULL,
-    "hire_date" string   NOT NULL,
+    "first_name" varchar(200)   NOT NULL,
+    "last_name" varchar(200)   NOT NULL,
+    "sex" varchar(200)   NOT NULL,
+    "hire_date" varchar(200)   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
@@ -49,24 +49,10 @@ CREATE TABLE "salaries" (
 
 CREATE TABLE "titles" (
     "title_id" varchar(200)   NOT NULL,
-    "title" string   NOT NULL,
+    "title" varchar(200)   NOT NULL,
     CONSTRAINT "pk_titles" PRIMARY KEY (
         "title_id"
      )
 );
 
-ALTER TABLE "department" ADD CONSTRAINT "fk_department_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "dept_manager" ("dept_no");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "dept_manager" ("emp_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "salaries" ("emp_no");
-
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "dept_manager" ("emp_no");
-
-ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_title_id" FOREIGN KEY("title_id")
-REFERENCES "employees" ("emp_title_id");
 
