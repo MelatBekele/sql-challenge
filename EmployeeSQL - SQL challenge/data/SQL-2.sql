d4948a7f30b7b5398b140e7c745860b01d57d6db
Melat Bekele <27melat28@gmail.com>
2021-06-27 02:09:05 -0400

    new files

--- EmployeeSQL - SQL challenge/data/SQL-2.sql
+++ EmployeeSQL - SQL challenge/data/SQL-2.sql
@@ -34,14 +34,13 @@ order by employees.hire_date
 Select
 dept_manager.dept_no,
 department.dept_name,
-dept_emp.emp_no,
+dept_manager.emp_no,
 employees.last_name, 
 employees.first_name 
 
 from dept_manager
 join department on dept_manager.dept_no = department.dept_no
-join dept_emp on dept_manager.dept_no = dept_emp.dept_no
-join employees on dept_emp.emp_no = employees.emp_no
+join employees on employees.emp_no = dept_manager.emp_no
 
 --4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 Select 
@@ -55,11 +54,49 @@ join dept_emp on employees.emp_no = dept_emp.emp_no
 --where employees.emp_no = '57444'
 
 --5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+Select  
+employees.first_name, 
+employees.last_name, 
+employees.sex
 
+from employees
+where employees.first_name ='Hercules' and employees.last_name like '%B%'
 
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
+Select 
+employees.emp_no, 
+employees.last_name,
+employees.first_name, 
+department.dept_name
 
+from employees
+join dept_emp on employees.emp_no = dept_emp.emp_no
+join department on dept_emp.dept_no = department.dept_no
 
+where department.dept_name = 'Sales'
+
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+Select 
+employees.emp_no, 
+employees.last_name,
+employees.first_name, 
+department.dept_name
+
+from employees
+join dept_emp on employees.emp_no = dept_emp.emp_no
+join department on dept_emp.dept_no = department.dept_no
+
+where department.dept_name = 'Sales' or department.dept_name = 'Development'
+
+--8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+Select  
+employees.last_name,
+count(employees.last_name) as count_of_lastname
+
+from employees
+group by employees.last_name
 
 
 
